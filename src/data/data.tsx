3340b503be89b5231916d5a17a67c1c91523afeb
jwkim <jeremywrightkim@gmail.com>
2025-09-08 16:23:47 +0900

    [fix] add history

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -28,7 +28,6 @@ import {
   Hero,
   HomepageMeta,
   PortfolioItem,
-  SkillGroup,
   TestimonialSection,
   TimelineItem,
 } from './dataDef';
@@ -50,7 +49,6 @@ export const SectionId = {
   Contact: 'contact',
   Portfolio: 'portfolio',
   Resume: 'resume',
-  Skills: 'skills',
   Stats: 'stats',
   Testimonials: 'testimonials',
 } as const;
@@ -96,8 +94,7 @@ export const heroData: Hero = {
 export const aboutData: About = {
   profileImageSrc: profilepic,
   description: `김광섭, 보산개발㈜ 대표이사, 1956년생.
-  1981년부터 부동산 중개 · 개발 업계에 몸담아 오며, 40년이 넘는 경험을 바탕으로 다양한 주거 및 상업시설 프로젝트를 성공적으로 이끌어왔습니다. 
-  이를 통해 보산개발㈜은 안정적인 사업 수행 능력과 신뢰성을 인정받고 있으며, 앞으로도 지역 발전과 도시가치 재고를 위한 혁신적인 개발사업을 지속적으로 추진할 계획입니다.`,
+  1981년부터 부동산 중개 · 개발 업계에 몸담아 오며, 40년이 넘는 경험을 바탕으로 다양한 주거 및 상업시설 프로젝트를 성공적으로 이끌어왔습니다. 이를 통해 보산개발㈜은 안정적인 사업 수행 능력과 신뢰성을 인정받고 있으며, 앞으로도 지역 발전과 도시가치 재고를 위한 혁신적인 개발사업을 지속적으로 추진할 계획입니다.`,
   aboutItems: [
     {label: 'Nationality', text: '대한민국', Icon: FlagIcon},
     {label: 'Company', text: '보산개발㈜', Icon: BuildingOffice2Icon},
@@ -259,41 +256,81 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2025-',
+    location: '부산 해운대구',
+    title: '해운대 하버타운 재건축 및 재개발 추진 중',
+  },
+    {
+    date: '2025-',
+    location: '부산 중구 광복동',
+    title: '광복동 1가 7일원 토지 1,741.22평 주상복합건설물 개발사업 컨설팅 진행중',
+  },
+    {
+    date: '-',
+    location: '부산, 반도, 동일, 김해 삼계동',
+    title: '부산, 반도 동일 김해 삼계동 APT부지매입 부동산 컨설팅 및 부지매입',
+  },
+    {
+    date: '2008-2011',
+    location: '부산 해운대구 우동',
+    title: '우동 651-2번지 해운대 그랜드호텔 매각 및 주상복합건물 개발사업 컨설팅 완료',
+    content: '지하 6층~지상 22층, 30,951평',
+  },
+    {
+    date: '',
+    location: '-',
+    title: 'PF사업 KB신탁, PF 340억 신탁해제, 하나캐피탈, 하나은행',
+  },
+    {
+    date: '2008',
+    location: '부산 해운대구 중동',
+    title: '중동 1417-2번지 상업용지 309평 해수욕장입구 근린생활시설 (씨스타빌딩) 사업시행 완료',
+    content: '건축연면적 1,959.67평, 용적율 880.81%, 규모 지하3층~지상15층',
+  },  
+   {
+    date: '2006.01',
+    location: '부산',
+    title: '`지앤에이치산업개발주식회사` 에서 `조은개발주식회사` 상호변경',
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2005',
+    location: '부산 송도 암남동',
+    title: '암남동 589-1번지 외 2,615평 송도 해수욕장 주변, 준주거지역 및 일반주거지역에 대한 지구단위 및 종 변경 공동주택 개발사업 컨설팅 완료',
   },
-];
-
-export const experience: TimelineItem[] = [
-  {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
+    {
+    date: '2005.01',
+    location: '부산 수영구 남천동',
+    title: '남천동 25-1번지 반도건설 계열회사, 반도 모터스㈜ 인피니티 자동차 전시장 컨설팅 완료',
+  },
+    {
+    date: '2003.07',
+    location: '부산 해운대구 우동',
+    title: '우동 862번지 일원 상업용지 3,000여평 아파트 개발사업 컨설팅 완료 (한신공영)',
+  },
+    {
+    date: '2003.01',
+    location: '부산',
+    title: '지앤에이치산업개발주식회사 부동산개발 및 주택건설업 창업',
+  },
+    {
+    date: '',
+    location: '',
+    title: '패션, 논노, 까뜨리네트, 조이너스, 한섬, 파크랜드 패션브랜드 유치 및 매장개설 및 화장품, 이동통신 매장 개설 및 대리점 유치 등',
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
+    date: '1981-2006',
+    location: '부산 중구 광복동',
+    title: '부동산 개발 및 중개업 종사',
+  },
+  {
+    date: '-',
+    location: '-',
+    title: '육군 병장 3년 만기전역',
+  },
+  {
+    date: '-',
+    location: '-',
+    title: '중/고등학교 축구선수 생활, 주장역임 중등부 최우수 선수상 수상',
   },
 ];
 
