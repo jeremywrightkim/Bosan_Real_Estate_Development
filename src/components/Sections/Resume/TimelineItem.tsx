1b23f11c0ae7a525d92b4dab87cd7da1f2a2e72b
jwkim <jeremywrightkim@gmail.com>
2025-09-08 16:47:59 +0900

    update TimelineItem.tsx

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -8,13 +8,15 @@ const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
       <div className="flex flex-col pb-4">
         <h2 className="text-xl font-bold">{title}</h2>
-        <div className="flex items-center justify-center gap-x-2 md:justify-start">
+        <div className="flex items-center justify-center gap-x-1 md:justify-start">
           <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
-          <span>•</span>
+          <span>/</span>
           <span className="flex-1 text-sm sm:flex-none">{date}</span>
         </div>
-      </div>
-      {content}
+        <div className="flex-2 items-center justify-center font-medium italic pb-4 text-sm center last:pb-0 md:text-left">
+        {content}
+        </div>
+      </div>      
     </div>
   );
 });
